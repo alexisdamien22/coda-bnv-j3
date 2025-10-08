379ab7ead4aac26c55a8ad8f300fb1d26d19cab1
alexisdamien <alexisdamien@icloud.com>
2025-10-08 08:23:28 +0000

    added exercice 3

---
+++ ex-3/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	char mot[50];
+	printf("Saisissez un mot de moins de 50 caractères :\n");
+	scanf("%s",mot);
+	int i = 0;
+	while(i<5)
+	{
+		i++;
+		printf("%d : %s\n",i,mot);
+	}
+}
